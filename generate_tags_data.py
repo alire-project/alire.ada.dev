4655fba4fb9c50ed37956fb34bfb90025be9c855
Jesper Quorning <jesper.quorning@gmail.com>
2022-03-19 00:46:06 +0100

    Unterse tags.json

--- generate_tags_data.py
+++ generate_tags_data.py
@@ -8,7 +8,15 @@ try:
 except ImportError:
     from yaml import Loader
 
-data = {}
+terse = {}
+# { tag_name_1: [ crate_1, crate_2 ],
+#   tag_name_2: [ crate_3, crate_4 ],
+#   ... }
+
+tags = []
+# [ { name: tag_name_1, crates: [ crate_1, crate_2 ] },
+#   { name: tag_name_2, crates: [ crate_2, crate_3 ] },
+#   ...} ]
 
 def extract_yaml(filename):
     print("loading %s" % filename)
@@ -42,14 +50,17 @@ for cratefile in glob.glob("_crates/*.md"):
         if 'tags' in ydata and ydata['tags']:
             crate_tags = ydata['tags']
             for tag in crate_tags:
-                if tag not in data:
-                    data[tag] = []
-                data[tag].append(crate)
+                if tag not in terse:
+                    terse[tag] = []
+                terse[tag].append(crate)
+
+for tag in terse:
+    tags.append({'name': tag, 'crates': terse[tag]})
 
 
 script_dir=os.path.dirname(os.path.realpath(__file__))
 
 with open(os.path.join(script_dir, "_data", 'tags.json'), 'w') as file:
-    out = json.dumps(data, indent=4)
+    out = json.dumps(tags, indent=4)
     print(out)
     file.write(out)
